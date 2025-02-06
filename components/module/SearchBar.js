fd0762044aa0f3859cc1840c721f4c5088c6c47d
unknown <amincxo@gmail.com>
2025-02-06 03:54:53 +0330

    serchHandler complate

--- components/module/SearchBar.js
+++ components/module/SearchBar.js
@@ -1,12 +1,28 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router';
+
 import styles from './SearchBar.module.css'
 
 function SearchBar() {
+    const router = useRouter()
+
+    const [min , setMin] = useState('');
+    const [max , setMax] = useState('');
+
+    const searchHandler = () => {
+        if (min && max) {
+            router.push(`/filter/${min}/${max}`)
+        } else {
+            alert("lotfan vard konid")
+        }
+    }
+
   return (
     <div className={styles.container} >
         <div>
-            <input  placeholder="Enter min-price" />
-            <input  placeholder="Enter max-price" />
-            <button>Search</button>
+            <input placeholder="Enter min-price" value={min} onChange={e => setMin(e.target.value)} />
+            <input placeholder="Enter max-price" value={max} onChange={e => setMax(e.target.value)} />
+            <button onClick={searchHandler} >Search</button>
         </div>
     </div>
   )
